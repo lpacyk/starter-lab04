613f0da83cec39e84dc52d5d8faeeb64b2a5dba1
Lukasz Pacyk <lpacyk31@gmail.com>
2015-10-18 22:28:55 -0700

    Checkout complete successfully. Outcome is as expected. Lab complete

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -18,7 +18,7 @@ class Orders extends MY_Model {
         if ($CI->orderitems->exists($num,$code))
         {
             $record = $CI -> orderitems -> get($num, $code);
-            $record -> quanitiy++;
+            $record -> quantity++;
             $CI -> orderitems -> update($record);
         } else
         {
@@ -57,7 +57,16 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+        $CI = & get_instance();
+        $items = $CI -> orderitems-> group($num);
+        $gotem = array();
+        if (count($items)>0)
+            foreach($items as $item)
+            {
+            $menu =$CI -> menu ->get($item->item);
+            $gotem[$menu->category] =1;
+            }
+        return isset($gotem['m']) && isset ($gotem['d']) && isset($gotem['s']);
     }
 
 }
